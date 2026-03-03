38c549b165d5f369b57e82d8e25849a414aec2a2
Arlen Freitas <arlenfreitas1@gmail.com>
2026-03-03 11:58:20 -0300

    test: Add Testes parametrizados para subtração (Red)

--- Documentos/Fase_02/Sprint_01/Semana_02/calculadora/tests/test_calculadora.py
+++ Documentos/Fase_02/Sprint_01/Semana_02/calculadora/tests/test_calculadora.py
@@ -24,3 +24,18 @@ class TestCalculadora:
         """Teste para o método somar."""
         resultado = calc.somar(a, b)
         assert resultado == esperado
+
+    """Teste para o método subtrair usando pytest.mark.parametrize para testar múltiplos casos."""
+    @pytest.mark.parametrize("a, b, esperado", [
+        (2, 3, -1),
+        (15, 10, 5),
+        (0, 0, 0),
+        (3.5, 2.5, 1.0),
+        (-5, -4, -1),
+        (5, -3, 8)
+    ])
+
+    def test_subtrair(self, calc, a, b, esperado):
+        """Teste para o método subtrair."""
+        resultado = calc.subtrair(a, b)
+        assert resultado == esperado
