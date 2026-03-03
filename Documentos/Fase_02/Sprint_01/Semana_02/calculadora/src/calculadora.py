a0a4ed84f43afe174d74652f6f5523f5af6c87a9
Arlen Freitas <arlenfreitas1@gmail.com>
2026-03-03 15:38:37 -0300

    feat: add Método subtrair (GREEN)

--- Documentos/Fase_02/Sprint_01/Semana_02/calculadora/src/calculadora.py
+++ Documentos/Fase_02/Sprint_01/Semana_02/calculadora/src/calculadora.py
@@ -5,3 +5,7 @@ class Calculadora:
     def somar(self, a, b):
         """Retorna a soma de a e b."""
         return a + b
+
+    def subtrair(self, a, b):
+        """Retorna a subtração de a por b."""
+        return a - b
